1d7f13ae44933372c8f5d95328ae1ff82a3b5301
mk-Guvi <61502453+mk-Guvi@users.noreply.github.com>
2021-02-27 16:03:52 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -2,19 +2,8 @@ import React from "react";
 import ReactDom from "react-dom";
 const root = document.getElementById("root");
 
-const h1 = React.createElement("h1", { className: "h1" }, "hello world!");
-const container = React.createElement(
-  "div",
-  { className: "conatiner" },
-  React.createElement("div", { className: "des" }),
-  React.createElement("p", { className: "text" }, "hello para")
-);
-const wel="hello jsx para"
-const jsx = (
-  <div className="container">
-    <div className="des">
-      <p className="text">hello jsx para {wel}</p>
-    </div>
-  </div>
-);
-ReactDom.render(jsx, root);
+const App = () => {
+  return <div>Hell dude</div>;
+};
+
+ReactDom.render(<App />, root);
