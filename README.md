4d56b9734b6f17172fda94cbf92b6d71865feb55
mk-Guvi <61502453+mk-Guvi@users.noreply.github.com>
2021-02-27 17:10:26 +0530

    till adding addtask function (6)

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# react-basics
+# react-basics + ToDoList App
+
 Created with CodeSandbox
+
+1.)create components folder
+
+2.)create a app component in index.js file
+
+3.)create input and button(+) fields init.
+
+4.)create the tasks array with objects and use map function that returns inidividual task in ul tag.
+
+5.)In app.js,imp0rt useState-hook and add value(inputText),onchange(inputchangehandler) attributes in input field.using usestate hook make the input text what we enter in keyboard.
+
+6.)add usestate to tasks and create a addtask function that updates the array with new task and call the addtask function in the onclick attribute of button.
--- src/index.js
+++ src/index.js
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
 const root = document.getElementById("root");
 
 const App = () => {
-  return <div>Hell dude</div>;
+  const [tasks, setTasks] = useState([
+    {
+      text: "Grocery",
+      isComplete: true
+    },
+    {
+      text: "TIffan",
+      isComplete: false
+    }
+  ]);
+
+  const addTask = (tasktext) => {
+    const newTask = {
+      text: tasktext,
+      isComplete: false
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  //since usestate is an array that has first element as default value and second as dispatch function(which allows u to change the state)
+  //inputText can only be changed by setInputText
+
+  const [inputText, setInputText] = useState("");
+  const inputChangeHandler = (e) => {
+    setInputText(e.target.value);
+  };
+  window.setInputText = setInputText;
+  return (
+    <div>
+      <h1>ToDo List App</h1>
+      <input
+        type="text"
+        value={inputText}
+        onChange={inputChangeHandler}
+        placeholder="Add New Task"
+      />
+      <button
+        onClick={() => {
+          addTask(inputText);
+        }}
+      >
+        +
+      </button>
+      <ul>
+        {tasks.map((task, taskIndex, originalArray) => {
+          return <li key={taskIndex}>{task.text}</li>;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 ReactDom.render(<App />, root);
