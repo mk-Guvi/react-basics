8829ca5affddf9dec1fa38df4f8a7c9c1d1c81a8
mk-Guvi <61502453+mk-Guvi@users.noreply.github.com>
2021-03-06 11:45:48 +0530

    componentdidcatch

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -18,11 +18,17 @@ class ErrorBoundary extends Component {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
+  // componentDidCatch to store the error and its details
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service like  Sentry, Bugsnag, Firebase crashlytics
+    //instead of console.log()   use the  error reporting services
+    console.log(error, errorInfo);
+  }
   render() {
     if (this.state.hasError) {
       //if anything goes wrong in the errorboundary component it will render the below
       //return <h1>SomeThing Went Wrong</h1>;
-     // return null//this will hide the component
+      // return null//this will hide the component
     } //if no error occurs return all the child elements directly
     return this.props.children;
   }
